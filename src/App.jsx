8f56999a706557c79215d0979a36c80f27aec5f5
Ben-Benston <burhanasikwala@gmail.com>
2025-04-15 18:04:09 +0530

    Fix Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./sections/Home"
-import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <Routes>
-      <Analytics />
       <Route path="/" element={<Home />} />
     </Routes>
   )
--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,9 +1,11 @@
 import Experience from "./Experience";
 import NameAbout from "./NameAbout";
+import { Analytics } from "@vercel/analytics/react"
 
 function Home() {
     return (
         <div>
+            <Analytics />
             <NameAbout />
             <Experience />
         </div>
