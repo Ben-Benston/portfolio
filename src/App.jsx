49f2e322f9739232ad39b57933c5c6dc74d1a00f
Ben-Benston <burhanasikwala@gmail.com>
2025-04-15 18:02:37 +0530

    Added Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./sections/Home"
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <Routes>
+      <Analytics />
       <Route path="/" element={<Home />} />
     </Routes>
   )
